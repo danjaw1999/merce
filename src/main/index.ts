d0c76f957597bcdbd3ad14a2c1d88c23ab6be2cb
Daniel Jaworski <daniel.jaworski@merce.com>
2020-11-18 15:44:09 +0100

    classes, extend

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,19 +1,73 @@
-class Animal {
-    private run: string;
-    private sound: string;
-    constructor(run: string, sound: string){
-        this.run = run;
-        this.sound = sound;
-    }
-        text(spanie = 'spać'){
-            console.log(`Zwierzątko ${this.run} i ${this.sound} i pójdzie ${spanie}`)
-        }
+        // console.log(hey("duder"), activeDate({ startDate: new Date("2018-12-01"), endDate: new Date("2018-12-10")}));
     
-}
-
-const pies = new Animal('bieganie', 'szczekanie');
-pies.text();
-
-
-
-
+    // class Animal {
+    // private sound :string;
+    // private move: string;
+    
+    // constructor(g: string = 'kfgjn', o: string = 'dkfjgnd') {
+    // this.sound = g;
+    // this.move = o;
+    // }
+    
+    // print() {
+    // console.log(${this.sound} , ${this.move});
+    // }
+    // }
+    
+    // ////
+    
+    // let asd = new Animal();
+    // let kot = new Animal('miał', 'szybko');
+    // let pies = new Animal('bark', 'wolno');
+    
+    
+    // asd.print()
+    // kot.print();
+    // pies.print();
+    
+    class Jajeczko {
+    constructor (theName: string, theSurname: string) {}
+    
+    klops(a: number) {
+    console.log('a', a)
+    }
+    }
+    class Animal extends Jajeczko {
+    isBarking: boolean = true;
+    
+    constructor (theName: string, theSurname: string) {
+    super(theName, theSurname);
+    }
+    
+    move(distanceInMeters: number = 0) {
+    console.log(`Animal moved ${distanceInMeters}m.`);
+    console.log('isBarking', this.isBarking);
+    }
+    }
+    
+    class Dog extends Animal {
+    // isBarking: boolean;
+    theSex: string;
+    
+    constructor (theName: string, theSurname: string, theSex: string) {
+    super(theName, theSurname);
+    this.theSex = theSex;
+    }
+    
+    bark() {
+    console.log("Woof! Woof!");
+    console.log(this.theSex);
+    }
+    
+    move(m: number) {
+    super.move(m)
+    console.log('hahahha')
+    }
+    }
+    
+    const dog = new Dog('Klops', 'jajecko', 'male');
+    dog.bark();
+    dog.move(10);
+    dog.bark();
+    dog.klops(9);
+    dog.isBarking = false;
